a0b4a15e05415fc0ee9d91558796db98b4a8e81a
Antti Lehtonen <anttileh@gmail.com>
2024-11-16 17:19:45 +0200

    Dropping stone.

--- doggo/src/main.rs
+++ doggo/src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 const WINDOW_WIDTH: f32 = 1024.0;
 const WINDOW_HEIGHT: f32 = 720.0;
@@ -8,62 +9,41 @@ const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.0;
 
 const HORSE_PATH: &str = "textures/horses.png";
 
+const PLATFORM_COLOR: Color = Color::srgba(0.0, 0.0, 0.0, 255.0);
+
+const FLOOR_THICKNESS: f32 = 10.0;
+
+const COLOR_FLOOR: Color = Color::rgb(0.45, 0.55, 0.66);
+
+const SKY_HEIGHT_PERCENT: f32 = 70.0;
+
+const GRASS_START_Y: f32 = WINDOW_BOTTOM_Y + (WINDOW_HEIGHT * 0.15);
 // Add a new struct for the Horse component
 #[derive(Component)]
 struct Horse;
 
-// Function to spawn the horse
-fn spawn_horse(
-    commands: &mut Commands,
-    asset_server: &Res<AssetServer>,
-    materials: &Res<Assets<Image>>,
-) {
-    let texture_handle = asset_server.load(HORSE_PATH);
-
-    commands.spawn(SpriteBundle {
-        texture: texture_handle,
-        ..Default::default()
-    })
-    .insert(Horse)
-    .insert(Transform::from_translation(Vec3::new(0.0, 0.0, 0.0))); // Position on grass
-}
-
-// Function to move the horse
-fn move_horse(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&Horse, &mut Transform)>,
-) {
-    for (_, mut transform) in query.iter_mut() {
-        if keyboard_input.pressed(KeyCode::ArrowLeft) {
-            transform.translation.x -= 1.0; // Move left
-        }
-        if keyboard_input.pressed(KeyCode::ArrowRight) {
-            transform.translation.x += 1.0; // Move right
-        }
-    }
-}
-
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Bevy Platformer".to_string(),
-                resizable: false,
-                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
-                ..Default::default()
-            }),
-            ..Default::default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Bevy Platformer".to_string(),
+                        resizable: false,
+                        resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .add(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.0))
+                .add(RapierDebugRenderPlugin::default()),
+        )
         .add_systems(Startup, setup)
-        .add_systems(Update, update_system)
+        // .add_systems(FixedUpdate, update_system)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    materials: Res<Assets<Image>>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Draw the sky
     commands.spawn(SpriteBundle {
         sprite: Sprite {
@@ -72,7 +52,11 @@ fn setup(
         },
         transform: Transform {
             translation: Vec3::new(0.0, WINDOW_HEIGHT * 0.5, 0.0), // Centered vertically
-            scale: Vec3::new(WINDOW_WIDTH, WINDOW_HEIGHT * 1.4, 1.0), // 70% height
+            scale: Vec3::new(
+                WINDOW_WIDTH,
+                WINDOW_HEIGHT * SKY_HEIGHT_PERCENT / 100.0 * 2.0,
+                1.0,
+            ), // 70% height
             ..Default::default()
         },
         ..Default::default()
@@ -85,62 +69,48 @@ fn setup(
             ..Default::default()
         },
         transform: Transform {
-            translation: Vec3::new(0.0, WINDOW_BOTTOM_Y + (WINDOW_HEIGHT * 0.15), 0.0), // Position it at the bottom
-            scale: Vec3::new(WINDOW_WIDTH, WINDOW_HEIGHT * 0.3, 1.0), // 30% height
+            translation: Vec3::new(0.0, GRASS_START_Y, 0.0), // Position it at the bottom
+            scale: Vec3::new(
+                WINDOW_WIDTH,
+                WINDOW_HEIGHT * (100.0 - SKY_HEIGHT_PERCENT) / 100.0,
+                1.0,
+            ), // 30% height
             ..Default::default()
         },
         ..Default::default()
     });
 
-    // Optionally, spawn other sprites (e.g., the rectangles you had before)
-    /*
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::LIME_GREEN,
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: PLATFORM_COLOR,
+                ..Default::default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0.0, 200.0, 0.0),
+                scale: Vec3::new(40.0, 40.0, 1.0),
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        transform: Transform {
-            translation: Vec3::new(-100.0, WINDOW_BOTTOM_Y + (200.0 / 2.0), 0.0),
-            scale: Vec3::new(75.0, 200.0, 1.0),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
+        })
+        .insert(RigidBody::Dynamic)
+        .insert(Collider::cuboid(0.5, 0.5));
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::LIME_GREEN,
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: COLOR_FLOOR,
+                ..Default::default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.5*GRASS_START_Y - 15.0, 0.0),
+                // scale: Vec3::new(WINDOW_WIDTH, FLOOR_THICKNESS, 1.0),
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        transform: Transform {
-            translation: Vec3::new(100.0, WINDOW_BOTTOM_Y + (350.0 / 2.0), 0.0),
-            scale: Vec3::new(50.0, 350.0, 1.0),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::LIME_GREEN,
-            ..Default::default()
-        },
-        transform: Transform {
-            translation: Vec3::new(350.0, WINDOW_BOTTOM_Y + (250.0 / 2.0), 0.0),
-            scale: Vec3::new(150.0, 250.0, 1.0),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-    */
+        })
+        .insert(RigidBody::Fixed)
+        .insert(Collider::cuboid(0.5, 0.5));
 
     commands.spawn(Camera2dBundle::default());
-    spawn_horse(&mut commands, &asset_server, &materials);
-}
-
-fn update_system(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&Horse, &mut Transform)>,
-) {
-    move_horse(keyboard_input, query); // Call the move_horse function
 }
