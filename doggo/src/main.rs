47ddf65105259e3f3ba130094cf0be2a9f3b9905
Antti Lehtonen <anttileh@gmail.com>
2024-11-16 16:15:38 +0200

    Horse stuff.

--- doggo/src/main.rs
+++ doggo/src/main.rs
@@ -6,6 +6,43 @@ const WINDOW_HEIGHT: f32 = 720.0;
 const WINDOW_BOTTOM_Y: f32 = WINDOW_HEIGHT / -2.0;
 const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.0;
 
+const HORSE_PATH: &str = "textures/horses.png";
+
+// Add a new struct for the Horse component
+#[derive(Component)]
+struct Horse;
+
+// Function to spawn the horse
+fn spawn_horse(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    materials: &Res<Assets<Image>>,
+) {
+    let texture_handle = asset_server.load(HORSE_PATH);
+
+    commands.spawn(SpriteBundle {
+        texture: texture_handle,
+        ..Default::default()
+    })
+    .insert(Horse)
+    .insert(Transform::from_translation(Vec3::new(0.0, 0.0, 0.0))); // Position on grass
+}
+
+// Function to move the horse
+fn move_horse(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&Horse, &mut Transform)>,
+) {
+    for (_, mut transform) in query.iter_mut() {
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            transform.translation.x -= 1.0; // Move left
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            transform.translation.x += 1.0; // Move right
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -18,10 +55,15 @@ fn main() {
             ..Default::default()
         }))
         .add_systems(Startup, setup)
+        .add_systems(Update, update_system)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    materials: Res<Assets<Image>>,
+) {
     // Draw the sky
     commands.spawn(SpriteBundle {
         sprite: Sprite {
@@ -93,4 +135,12 @@ fn setup(mut commands: Commands) {
     */
 
     commands.spawn(Camera2dBundle::default());
+    spawn_horse(&mut commands, &asset_server, &materials);
+}
+
+fn update_system(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&Horse, &mut Transform)>,
+) {
+    move_horse(keyboard_input, query); // Call the move_horse function
 }
